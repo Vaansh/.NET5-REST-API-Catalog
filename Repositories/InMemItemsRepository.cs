5857f7a2c490ee2a4a8d6f9bb7c16e69fa203ee7
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2020-12-31 20:51:51 +0400

    Dependency Injection, DTOs
    Register and inject dependencies in .NET 5 and implement Data Transfer Objects (DTOs)

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using Catalog.Entities;
 
 namespace Catalog.Repositories
-{
-    public class InMemItemsRepository
+{    
+    public class InMemItemsRepository : IItemsRepository
     {
         private readonly List<Item> items = new()
         {
             new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
             new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
-            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }            
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
         };
         public IEnumerable<Item> GetItems()
         {
@@ -22,5 +22,5 @@ namespace Catalog.Repositories
         {
             return items.Where(item => item.Id == id).SingleOrDefault();
         }
-    }    
+    }
 }
